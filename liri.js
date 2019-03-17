cc7e7733dcddc5b8b40b4a80c6c137226ba8ad25
Kgor <kevin.gorham@gmail.com>
2019-03-16 23:09:05 -0700

    got movie-this finshed

--- liri.js
+++ liri.js
@@ -8,6 +8,19 @@ var command = process.argv[2];
 var searchTerm = '';
 var searchSpaced = '';
 var date = '';
+
+var logMovieData = function (response) {
+    console.log("-------------------- Response --------------------");
+    console.log("Title: " + response.data.Title);
+    console.log("Year released: " + response.data.Year);
+    console.log("IMDB Rating: " + response.data.imdbRating);
+    console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
+    console.log("Country produced: " + response.data.Country);
+    console.log("Language: " + response.data.Language)
+    console.log("Plot: " + response.data.Plot);
+    console.log("Actors: " + response.data.Actors);
+    console.log("----------------------- End ----------------------")
+};
 var createSearchTerm = function () {
     for (var i = 3; i < process.argv.length; i++) {
         searchTerm += process.argv[i];
@@ -79,51 +92,46 @@ var movieThis = function () {
         axios
             .get(movieUrl)
             .then(function (response) {
-                console.log("-------------------- Response --------------------");
-                console.log("Title: " + response.data.Title);
-                console.log("Year released: " + response.data.Year);
-                console.log("IMDB Rating: " + response.data.imdbRating);
-                console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
-                console.log("Country produced: " + response.data.Country);
-                console.log("Language: " + response.data.Language)
-                console.log("Plot: " + response.data.Plot);
-                console.log("Actors: " + response.data.Actors);
-                console.log("----------------------- End ----------------------")
+                logMovieData(response);
             });
-
+    } else {
         // If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-        // You'll use the axios package to retrieve data from the OMDB API. Like all of the in-class activities, the OMDB API requires an API key. You may use trilogy.
+        axios
+            .get(mrNobodyUrl)
+            .then(function (response) {
+                logMovieData(response);
+            });
     };
-    // var doIt                = function () {};
+
 };
+// var doIt                = function () {};
 
+console.log(command);
 
-    console.log(command);
+moment().format();
+createSearchTerm();
+createSearchTermSpaced();
 
-    moment().format();
-    createSearchTerm();
-    createSearchTermSpaced();
+var bandsUrl = "https://rest.bandsintown.com/artists/" + searchTerm + "/events?app_id=codingbootcamp";
+var movieUrl = "http://www.omdbapi.com/?apikey=trilogy&t=" + searchSpaced
+var mrNobodyUrl = "http://www.omdbapi.com/?apikey=trilogy&t=mr.nobody"
 
-    var bandsUrl = "https://rest.bandsintown.com/artists/" + searchTerm + "/events?app_id=codingbootcamp";
-    var movieUrl = "http://www.omdbapi.com/?apikey=trilogy&t=" + searchSpaced
-
-
-    switch (command) {
-        case "concert-this":
-            concertThis();
-            break;
-        case "spotify-this-song":
-            spotifyThis();
-            break;
-        case "movie-this":
-            movieThis();
-            break;
-        case "do-what-it-says":
-            doIt();
-            break;
-        default:
-            console.log("Input is in incorrect format");
-    };
+switch (command) {
+    case "concert-this":
+        concertThis();
+        break;
+    case "spotify-this-song":
+        spotifyThis();
+        break;
+    case "movie-this":
+        movieThis();
+        break;
+    case "do-what-it-says":
+        doIt();
+        break;
+    default:
+        console.log("Input is in incorrect format");
+};
 
 
 
